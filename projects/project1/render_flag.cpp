528dc8c298d6832f7d82f02dd166c424835c3063
maksymilan <3510040005@qq.com>
2025-10-05 14:54:49 +0800

    project 1

--- projects/project1/render_flag.cpp
+++ projects/project1/render_flag.cpp
@@ -28,6 +28,41 @@ RenderFlag::RenderFlag(const Options& options) : Application(options) {
     // _stars[i].reset(new Star(ndc_position, rotation_in_radians, size_of_star,
     // aspect_of_the_window));
     // ---------------------------------------------------------------
+
+    float aspect = static_cast<float>(_windowWidth) / static_cast<float>(_windowHeight);
+    const float grid_width = 30.0;
+    const float grid_height = 20.0;
+
+    glm::vec2 large_star_pos(5.0,5.0);
+    float large_star_radius = 3.0;
+    glm::vec2 small_star_positions[] = {
+        glm::vec2(10.0f, 2.0f),
+        glm::vec2(12.0f, 4.0f),
+        glm::vec2(12.0f, 7.0f),
+        glm::vec2(10.0f, 9.0f)
+    };
+    float small_star_radius = 1.0;
+    auto map_to_NDC = [&](const glm::vec2& grid_pos) -> glm::vec2{
+        float ndcX = (grid_pos.x / grid_width) * 2 - 1;
+        float ndcY = -((grid_pos.y / grid_height) * 2 - 1);
+        return {ndcX,ndcY};
+    };
+    float large_star_ndc_radius = large_star_radius / grid_height;
+    float small_star_ndc_radius = small_star_radius / grid_height;
+    
+    glm::vec2 large_star_ndc_pos = map_to_NDC(large_star_pos);
+    _stars[0].reset(new Star(large_star_ndc_pos,90,large_star_ndc_radius,aspect));
+
+    for(int i=0;i<4;++i){
+        glm::vec2 cur_small_star_pos = small_star_positions[i];
+        glm::vec2 cur_small_star_ndc_pos = map_to_NDC(cur_small_star_pos);
+
+        glm::vec2 direction = large_star_pos - cur_small_star_pos;
+        float angle = atan2(direction.y,direction.x);
+
+        float rotation = angle - glm::radians(90.0);
+        _stars[i+1].reset(new Star(cur_small_star_ndc_pos,rotation,small_star_ndc_radius,aspect));
+    }
 }
 
 void RenderFlag::handleInput() {
--- projects/project1/star.cpp
+++ projects/project1/star.cpp
@@ -1,5 +1,6 @@
 #include "star.h"
 #include <cmath>
+#include <vector>
 
 Star::Star(const glm::vec2& position, float rotation, float radius, float aspect)
     : _position(position), _rotation(rotation), _radius(radius) {
@@ -10,7 +11,47 @@ Star::Star(const glm::vec2& position, float rotation, float radius, float aspect
     //     _vertices.push_back( ... );
     // }
     // -------------------------------------
+    const int num_points = 5;
+    const float radius_ratio = 0.38196601125f;
+    float inner_radius = _radius * radius_ratio;
 
+    std::vector<glm::vec2> outer_points(num_points);
+    std::vector<glm::vec2> inner_points(num_points);
+
+    float start_angle = glm::radians(_rotation);
+    float angle_step = 2 * M_PI / num_points;
+
+    for(int i=0;i<num_points;++i){
+        float outer_angle = start_angle + i * angle_step;
+        outer_points[i] = {_radius * cos(outer_angle) / aspect, _radius * sin(outer_angle)};
+
+        float inner_angle = outer_angle + angle_step / 2;
+        inner_points[i] = {inner_radius * cos(inner_angle) / aspect, inner_radius * sin(inner_angle)};
+    }
+
+    // assemble the inner pentagon by 3 triangles
+    _vertices.push_back(inner_points[0]);
+    _vertices.push_back(inner_points[1]);
+    _vertices.push_back(inner_points[2]);
+
+    _vertices.push_back(inner_points[0]);
+    _vertices.push_back(inner_points[2]);
+    _vertices.push_back(inner_points[3]);
+
+    _vertices.push_back(inner_points[0]);
+    _vertices.push_back(inner_points[3]);
+    _vertices.push_back(inner_points[4]);
+
+    // assemble the corner points, each point is composed of a triangle
+    for(int i=0;i<num_points;++i){
+        _vertices.push_back(outer_points[i]);
+        _vertices.push_back(inner_points[i]);
+        _vertices.push_back(inner_points[(i + num_points - 1)%num_points]);
+    }
+
+    for(auto& vertex : _vertices){
+        vertex += _position;
+    }
     glGenVertexArrays(1, &_vao);
     glGenBuffers(1, &_vbo);
 
